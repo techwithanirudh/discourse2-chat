7a40fc623c88b9b2644a6bb6e2ba6ceaa3a6331d
Tech With Anirudh <70373803+techwithanirudh@users.noreply.github.com>
2025-05-23 04:46:56 +0000

    fix(pkg): update OpenAPI schema (2025-05-23; "2496908")

--- src/schema.d.ts
+++ src/schema.d.ts
@@ -7151,6 +7151,8 @@ export interface operations {
                     /** @example hello world */
                     message?: string;
                     staged_id?: string | null;
+                    in_reply_to_id?: string | null;
+                    thread_id?: string | null;
                 };
             };
         };
